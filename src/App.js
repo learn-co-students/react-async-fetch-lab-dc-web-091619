eac1664b6e520ffc71ed8a0f9c8e310f60c1432a
Graham Flaspoehler <grahamflas@gmail.com>
2020-01-27 11:48:58 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React from 'react'
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      peopleInSpace: []
+    };
+  }
+
+  componentDidMount(){
+    fetch("http://api.open-notify.org/astros.json")
+      .then(resp => resp.json())
+      .then(astroObj => this.setState({
+        peopleInSpace: astroObj.people
+      }))
+  }
+
+  render(){
+    return(
+      <div>
+        {
+          this.state.peopleInSpace.map( ({name, craft}) => {
+            return <div>{`${name} is aboard the ${craft}.`}</div>
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default App
